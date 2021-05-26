2e99440c41fda361bf3ded1b0d99b62c026210fd
JiaYong-Chen <56154212+JiaYong-Chen@users.noreply.github.com>
2021-05-26 14:38:02 +0800

    Update getposition.py
    Keep reading the position data

--- getposition.py
+++ getposition.py
@@ -6,7 +6,6 @@ import requests
 import argparse
 import json
 
-
 def get_data(url):
     try:
         r = requests.get(url)
@@ -23,30 +22,27 @@ def get_data(url):
 def get_acoustic_position(base_url):
     return get_data("{}/api/v1/position/acoustic/filtered".format(base_url))
 
-
 def get_global_position(base_url):
     return get_data("{}/api/v1/position/global".format(base_url))
 
-
 def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-u', '--url', help='Base URL to use', type=str, default='http://demo.waterlinked.com')
-    args = parser.parse_args()
+    while 1:
+        parser = argparse.ArgumentParser(description=__doc__)
+        parser.add_argument('-u', '--url', help='Base URL to use', type=str, default='http://192.168.2.94')
+        args = parser.parse_args()
 
-    base_url = args.url
-    print("Using base_url: %s" % args.url)
+        base_url = args.url
+        print("Using base_url: %s" % args.url)
 
-    data = get_acoustic_position(base_url)
-    if data:
-        #print(data)
-        print("Current acoustic position {},{},{}".format(data["x"], data["y"], data["z"]))
+        data = get_acoustic_position(base_url)
+        if data:
+            #print(data)
+            print("Current acoustic position {},{},{}".format(data["x"], data["y"], data["z"]))
 
-    pos = get_global_position(base_url)
-    if pos:
-        #print(pos)
-        print("Current global position lat:{} lon:{}".format(pos["lat"], pos["lon"]))
-
-
+        pos = get_global_position(base_url)
+        if pos:
+            #print(pos)
+            print("Current global position lat:{} lon:{}".format(pos["lat"], pos["lon"]))
 
 if __name__ == "__main__":
     main()
